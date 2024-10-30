73a8267a473798e1a0ff3222a72694319a80a530
maryam <jakanmaryam21@gmail.com>
2024-10-30 12:43:02 +0500

    tapsyrma

---
+++ examples/30.10/const.js
@@ -0,0 +1,11 @@
+var name = "aty" // ozgeretin man
+
+let surname = "familia"; // string
+
+surname = "abc"; // surname ainymalynyn mani ozgeredi
+
+const id = 939848380; // turaaqty man/number
+
+id = 93842288; // qate qaitarady
+
+console.log(id); // sjqpaidy
