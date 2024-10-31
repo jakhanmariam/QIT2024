44198741b4f46feb5a9780f85a8bd4f8f21e5f6a
maryam <jakanmaryam21@gmail.com>
2024-10-31 12:14:15 +0500

    task1

---
+++ examples/31.10/script.js
@@ -0,0 +1,73 @@
+// let x = 4;
+
+// let y = 10;
+
+// let result = x+y; 
+
+// console.log(result);
+
+var a = 30;
+var b = 20;
+
+console.log(a - b);
+
+var x = 45;
+var y = 5;
+
+var result = x * y; 
+
+console.log(result);
+
+var c = 20;
+var g = 2;
+
+console.log(20 / 2);
+
+var a = 21;
+
+if (a % 2 == 0) {
+    console.log(a, "sany jup san");
+    
+}
+
+var num = 100;
+
+num++; // increment
+
+num--; // decrement
+
+// console.log(num);
+
+var a = 100;
+
+a += 5; // qosyp tagayinday a = a +5;
+
+a -= 5; // a = a -5
+
+a /= 5; // a = a /5
+
+a *= 5; // a = a *5
+
+a %= 3; // a = a %3
+
+console.log(a);
+
+var a = 1;
+
+var b = 8;
+
+var c = 3;
+
+var d = 9;
+
+var e = 5;
+
+let total = (a + b) * c - d / e;
+
+console.log(total);
+
+
+
+
+
+
---
+++ examples/31.10/tapsyrma.js
@@ -0,0 +1,11 @@
+var a = 8;
+
+var b = 3;
+
+var c = 8;
+
+var z = 19;
+
+let res = ((a * b)/ c) / (z - 3);
+
+console.log(res%2);
