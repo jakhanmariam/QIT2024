231e51769f132fbe8953db5696fb0f30998c828d
maryam <jakanmaryam21@gmail.com>
2024-11-02 13:32:19 +0500

    02.11 hw

---
+++ homeworks/28.10/02.11/script.js
@@ -0,0 +1,34 @@
+// 3 tapsyrma
+
+let planHeight = 10; 
+
+planHeight += 2;
+
+console.log(planHeight);
+
+// 2 tapsyrma
+
+let numberOfFriends = 6;
+
+let totalChocolates = 24;
+
+var a = 24;
+
+var b = 6;
+
+let chocolatesPerFriend = a/b;
+
+console.log(chocolatesPerFriend);
+
+// 1 tapsyrma
+
+let slicesPerPizza = 8;
+
+let numberOfPizza = 5;
+
+var c = 8;
+
+var d = 5;
+
+console.log(8*5);
+
