fc8a9b7f713c5fbaeb0b75824eb8d78925de16ce
maryam <jakanmaryam21@gmail.com>
2024-11-05 12:28:06 +0500

    05.11

---
+++ homeworks/28.10/05.11/script.js
@@ -0,0 +1,36 @@
+// 1 tapsyrma
+let age = 19;
+if (age > 16) {
+    console.log(age, true);
+} else {
+    console.log(age, false);
+}
+// 2 tapsyrma
+var num = 18;
+if (num % 2 == 0) {
+    console.log(jup);
+} else {
+    console.log(tak);
+}
+// 3 tapsyrma
+const temperature = 26.5;
+if (temperature >= 20 && temperature <= 30) {
+    console.log("temperature 20-30");
+} else {
+    console.log(false);
+}
+// 4 tapsyrma
+var num = 67;
+if (num > 0) {
+    console.log(ten);
+} else {
+    console.log(zero);
+    
+}
+// 5 tapsyrma
+var num = 90;
+if (num % 5 == 0 && num % 10 == 0) {
+    console.log(true);
+} else {
+    console.log(false);
+}
