a5d7de2d1d431fb2b1770b6fc65239f2bf0c08ce
maryam <jakanmaryam21@gmail.com>
2024-11-05 19:52:30 +0500

    task05.11

---
+++ examples/05.11/script.js
@@ -0,0 +1,91 @@
+// alert("Hello world!" + "Hello" + "\n" + "2024");
+
+// prompt("How are you?");
+
+// let name = prompt("Enter your name");
+// let age = prompt("enter your age");
+// let email = prompt("enter your email");
+
+// alert("Hello, " + name + "\n" + "your age " + age + "\n" + email)
+
+// alert("Hello," + name);
+
+// var num = prompt("enter any number: ");
+
+// if (num % 2 == 0) {
+//     alert("number " + num + " is even")
+// } else {
+//     alert("number " + num + " is odd")
+// }
+
+// var bitrhYear = prompt("enter year of birth:");
+// var age = "your age " + (2024 - bitrhYear);
+
+// if (isNaN(bitrhYear)){
+//     alert("enter a number");
+// } else {
+//     alert(age);
+// }
+
+// NAN - not a number
+
+// var password = prompt("enter a password");
+// var correctPassword = prompt("confirm password: ")
+
+// if (rym19 == correctPassword) {
+//     alert("welcome")
+// } else {
+//     alert("password is wrong!")
+// }
+
+// var userAns1 = prompt("Жібек сауда жолына «ұлы» сөзінің қосылу себебі:");
+// if (userAns1 == "Шығыс пен Батысты байланыстырды") {
+//     score++;
+//     alert("+1");
+// } else {
+//     alert("qate")
+// }
+// var userAns2 = prompt("Византия шеберлерінің жасаған күміс құмыралар табылған қала:");
+// if (userAns2 == "Қытай жеріндегі Хуанхэ өзенінің аңғарынан" || "Хуанхэ өзені") {
+//     score++;
+//     alert("+1");
+// } else {
+//     alert("qate")
+// }
+// var userAns3 = prompt("Жібек жолының халықаралық қарым - қатынас жағынан дами бастаған кезеңі:");
+// if (userAns3 == "қытай немесе болгар бұрышы" || "болгар бұрышы" || "қытай бұрышы") {
+//     score++;
+//     alert("+1");
+// } else {
+//     alert("qate")
+// }
+// var userAns4 = prompt(" Қытай императоры жібектен жасалған әшекейлі киімдерді сыйлық ретінде жіберді:");
+// if (userAns4 == "Соғды") {
+//     score++;
+//     alert("+1");
+// } else {
+//     alert("qate")
+// }
+// var userAns5 = prompt("Ұлы Жібек жолы қатынасының әлсірей бастауына әсер еткен басты оқиға:");
+// if (userAns5 == "13 ж. кейін") {
+//     score++;
+//     alert("+1");
+// } else {
+//     alert("qate")
+// }
+// var score = 0;
+
+// alert("Siz " + score + " jinadiniz!")
+
+// 239 F
+
+var temperature = prompt("enter temperature in Farenheit: ");
+var celsius = (temperature / 33.8);
+alert(Math.round(celsius));
+
+
+
+
+
+
+
