7e5f624db09ab113426b5583e13da996711509dc
maryam <jakanmaryam21@gmail.com>
2024-11-05 09:56:10 +0500

    tasks

---
+++ examples/03.11/script.js
@@ -0,0 +1,107 @@
+// var a = 20;
+// var b = 100;
+
+// console.log(a == b); // ten ekenin k
+// console.log(a > b); // salystyr
+// console.log(a < b);
+// console.log(a = b); // ! = ten emes
+
+// var a = 5
+
+// var b = 3
+
+// console.log(a >= b); //true 
+
+// var a = 5
+// var b = 3
+// var c = 4
+
+// console.log(a > b) && (c < a);
+
+//  var a = 5
+//  var b = 3 
+
+//  console.log(a + b < 4 || a > 0);
+
+//  var isOfRoad = false;
+
+//  console.log(!isOfRoad); 
+
+//  console.log(3 = 4); // ten emes // true
+
+//  // let age = 19;
+//  let hasTicket = true;
+//  let isVIP = false;
+
+//  if (( age >= 18 && hasTicket) || isVIP) {
+//     console.log("ruqsat");
+//  } else {
+//     console.log("bolmaydy");
+//  }
+
+//  var engine = 2.0;
+//  var color = 'black';
+//  var year = 2012;
+
+//  if (color == 'black' && year >= 2012) {
+//     console.log(jaraydy);
+//  } else if (engine > 0.2) {
+//     console.log(ok);
+//  } else {
+//     console.log(jaramaydy);
+//  }
+
+ 
+// // var age = 30;
+
+// console.log(age > 12 && age < 45);
+
+// var n = 21;
+
+// console.log(n % 2 == 0 && (age > 12 && age < 45));
+
+// var a = 10;
+// var b = 7;
+// var p = (a + b) * 2;
+
+// console.log((a == b) && (p >= 30 && p <= 40));
+
+// var a = 19;
+// var b = 1;
+
+// console.log(a % 3 == 0 || b % 5 == 0);
+
+// var temp1 = 10;
+// var tempo2 = 10;
+
+// console.log(temp1 >= 15 && temp1 <= 25 && temp2 > 30);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+
+
+
+
