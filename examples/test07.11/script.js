cc447f92a90be5b6ce78cfd86cbb66931f87f2a1
maryam <jakanmaryam21@gmail.com>
2024-11-07 12:26:20 +0500

    test

---
+++ examples/test07.11/script.js
@@ -0,0 +1,11 @@
+let num1 = prompt ("bir san tandaniz");
+let num2 = prompt ("bir san tandaniz");
+let operation = prompt ("-,+,/,*")
+
+switch (operation) {
+    case "-":
+        alert(num1-num2);
+        break;
+}
+
+
