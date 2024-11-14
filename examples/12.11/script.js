4a52935c97076d3aa52abdbb5364077052538def
maryam <jakanmaryam21@gmail.com>
2024-11-14 11:18:23 +0500

    tasks

---
+++ examples/12.11/script.js
@@ -0,0 +1,86 @@
+// for (let i = 1; i <= 5; i++ ) {
+//     console.log(i);
+// }
+
+// for (let i = 1; i <= 10; i++){
+//     if (i % 2 === 0) {
+//         console.log(i + " - jup ");
+//     } else {
+//         console.log(i + " - tak");
+//     }
+// } 
+
+// for (let i = 1; i <= 5; i ++){
+//     if (i == 3) {
+//         break;
+//     }
+//     console.log(i); // 1, 2, ...
+// }
+
+// for (let i = 10; i >= 1; i --){
+//     console.log(i);
+// }
+
+// let num = +prompt("enter number")
+
+// for (let i = 0; i < num; i += 2){
+//     console.log(i);
+// }
+
+// let num = +prompt("enter number")
+// let result = 0;
+
+// for (let i = 0; i <= num; i++){
+//     result += i;
+// }
+
+
+// for (let i = 1; i <= 3; i++) {
+//     let question = +prompt("enter number");
+
+//     if (question == 1456) {
+//         break;
+//     } else {
+//         alert("qate")
+//     }
+// }
+
+// let num = +prompt ("enter number");
+
+// for(let i = 1; i <= 10; i++) {
+//     console.log(i * num);
+// }
+
+// var numbers = [1, 2, 3, 4, 5];
+
+// for (let i = 1; i <= 5; i++){
+//     console.log(numbers[i]);
+// }
+
+// var numbers = [2,3,5,8,11,19,20,34,56];
+
+// for (let i = 1; i <=5; i++) {
+//     if (numbers[i] % 2 =/ 0){
+//         console.log();
+        
+//     }
+// }
+
+for (let i = 1; i <= 30; i++) {
+    if (i > 25 && i < 25)
+        continue;
+    console.log(i);
+}
+
+for (let i = 5; i <= 20; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+} 
+}
+
+
+
+
+
+
+
---
+++ examples/13.11/script.js
@@ -0,0 +1,86 @@
+// let apple = 1;
+
+// while (apple < 10) {
+//     console.log(apple + " apple");
+
+//     apple ++;
+// }
+
+// console.log("aiaqtadym");
+
+
+// let apple = 1;
+
+// while (apple < 10) {
+//     apple ++;
+//     if (apple % 2 == 0) {
+//         continue;
+//     }
+//     console.log(apple + " apple");
+// }
+
+// console.log("aiaqtadym");
+
+// let apple = 0;
+
+// while (apple < 30) {
+//     apple++;
+//     if (apple % 6 == 0) {
+//         console.log("sixth apple");
+//     }
+//     console.log(apple + "six apple");
+// }
+
+// let num = 1;
+// while (num <= 15) {
+//     if (num % 3 == 0 && num % 5 == 0){
+//         console.log("fizzBuzz");
+//     } else if (num % 3 == 0) {
+//         console.log("fizz");
+//     } else if (num % 5 == 0) {
+//         console.log("buzz");
+//     } else {
+//         console.log(num);
+//     }
+//     num++;
+// }
+
+// let num = 0;
+// let result = 0;
+
+// while (num <= 10) {
+//     result += num;
+// }
+
+// do{
+//     var number = prompt('on san engiziniz');
+// } while (number <= 0);
+
+// console.log("san" + number);
+
+// let num = 1;
+
+// while (num <= 90) {
+//     num++
+//     if (num % 2 == 0) {
+//         continue;
+//     }
+//     console.log("fizz");
+// }
+
+let score = 5;
+let correctAnswer = ("1465");
+let question;
+
+do {
+    question = prompt('kazak khandygy kashan kuryldy');
+} while (question != correctAnswer);
+
+console.log("");
+
+
+
+
+
+ 
+
