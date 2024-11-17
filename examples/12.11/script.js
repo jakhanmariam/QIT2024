5227de22ed754d7444116802721281ee916c676f
maryam <jakanmaryam21@gmail.com>
2024-11-17 17:13:34 +0500

    task

--- examples/12.11/script.js
+++ examples/12.11/script.js
@@ -62,7 +62,6 @@
 // for (let i = 1; i <=5; i++) {
 //     if (numbers[i] % 2 =/ 0){
 //         console.log();
-        
 //     }
 // }
 
---
+++ examples/14.11/script.js
@@ -0,0 +1,75 @@
+// let grade = 0;
+// let num = 0;
+// let result = 0;
+
+// do {
+//     grade = +prompt("")
+//     result = result + grade
+//     num++
+// } while (num <= 12)
+
+// console.log(result / 12);
+
+// resultEven = 0;
+// resultOdd = 0;
+// let num1 = +prompt("");
+// let num2 = +prompt("");
+// let = maxNum;
+// let = minNum;
+
+// if (num1 > num2) {
+//     maxNum = num1;
+//     minNum = num2;
+// } else {
+//     maxNum = num2;
+//     minNum = num1;
+// }
+
+
+// for (let i = minNum; i <= maxNum; i++) {
+//     if (i % 2 == 0) {
+//         resultEven += i;
+//     } else {
+//         resultOdd += i;
+//     }
+// }
+
+// console.log(`tak sandar kosyndysy ${resultOdd} osyndai`);
+// console.log(`jup sandar kosyndysy ${resultEven} osyndai`);
+
+
+// for (let i = 1; i <= 30; i++) {
+//     if (i % 3 == 0) {
+//         console.log(i);
+//     }
+// }
+
+// for (let i = 50; i >= 0; i--) {
+//     if (i % 5 == 0) {
+//         console.log(i);
+//     }
+// }
+
+// for (let i = 1; i <= 15; i++) {
+//     if (i % 4 == 0) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+for (let i = 1; i <= 20; i++) {
+    if (i % 3 == 0) {
+        console.log("Fizz");
+    } else if (i % 5 == 0) {
+        console.log("Buzz");
+    } else (i % 3 == 0 && i % 5 == 0)
+        console.log("FizzBuzz") 
+        break;
+    } 
+
+
+
+
+
+
+
