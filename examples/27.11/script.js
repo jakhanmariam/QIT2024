a22b4e13c4eff2dfc8d7620fd89698f41bbd309e
maryam <jakanmaryam21@gmail.com>
2024-11-27 18:46:23 +0500

    tasks

---
+++ examples/27.11/script.js
@@ -0,0 +1,151 @@
+// let cities = ["almaty","astana","semei","shymkent"];
+
+// cities.push("aktau");
+
+// cities[3] = "kokshetau";
+
+// console.log(cities);
+
+// cities.pop();
+
+// console.log(cities);
+
+// console.log(cities[2]);
+
+// console.log(cities[0]);
+
+// console.log(cities.length);
+
+// console.log(cities[cities.length -1]);
+
+// cities[cities.length - 1] = "oral";
+
+
+// let a = "almaty";
+// let b = a;
+// b = "aktau";
+// console.log(a);
+// console.log(b);
+
+
+// let arr1 = [1,2,3,4,5];
+// let arr2 = arr1;
+// arr2[0] = 0;
+// console.log(arr1);
+// console.log(arr2);
+// arr1.pop();
+// console.log(arr2);
+// console.log(arr1);
+
+// let  numbers = [5, 2, 8, 11, 1];
+// numbers.sort();
+// console.log(numbers);
+
+// numbers.sort((a, b) => a - b);
+// console.log(numbers);
+
+// numbers.reverse(); // massivety keri aynaldyrady
+// console.log(numbers);
+
+// numbers.slice(); // [5, 2, 8, 11, 1]
+// numbers.slice(2); //[8, 11, 1]
+// let slicedArray= numbers.slice(1, 3); // [2, 8]
+// console.log(slicedArray);
+
+
+// let numbers = [5, 2, 8, 11, 1];
+// numbers.splice(2, 3, 5);
+// console.log(numbers);
+
+// let numbers = [5, 2, 8, 11, 1];
+// numbers.includes(7); /// false
+
+
+// if  (numbers.includes(1)) {
+//     console.log("found 7 in the array");
+// } else {
+//     console.log("not in the array");
+// } 
+
+// // let inp = +prompt("enter any number");
+
+// if  (numbers.includes(inp)) {
+//     console.log("found 7 in the array"); // true
+// } else {
+//     console.log("not in the array"); // false
+// }
+
+// let cities = ["almaty","astana","semei","shymkent"];
+
+// let ind = cities.indexOf(""); // jok bolsa -1 bar bolsa indexin 
+// console.log(ind);
+
+
+// let inpCity = prompt("enter city");
+
+// // let cities = ["almaty","astana","semei","shymkent"];
+
+// if (cities.indexOf(inpCity) >= 0) {
+//     alert(inpCity + "is a city in our list");
+// } else {
+//     alert(inpCity + "is not a city");
+// }
+
+// let worldCities = ["idk","idk","tokyo","oslo"];
+
+// let kazCities = ["almaty","astana","semei","shymkent"];
+
+// let cities = worldCities.concat(kazCities) // kosady
+// console.log(cities);
+
+// let cities = worldCities.concat(kazCities, worldCities) // topluyor, iki kere yazdigimiz icin birinin kopyasini cikariyo
+// console.log(cities);
+
+// let cities = String(kazCities); // massivety stringe aynaldyru
+// console.log(cities);
+// console.log(cities[15]); // 15 ci harfi cikariyo
+
+// let kazCities = ["almaty","astana","semei","shymkent"];
+// let cities = kazCities.join("-"); // , yerine - oluyor
+// console.log(cities);
+
+// let joinedCities = "almaty, semei, shymkent";
+// let separatCities = joinedCities.split(",");
+// console.log(separatCities);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
